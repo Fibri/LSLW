0678b485f0eb8b563fd85e5b66498956cd8d8426
DGroeg <DGroeg@users.noreply.github.com>
2014-12-17 11:55:11 +0100

    Update lolipooo.py

--- lolipooo.py
+++ lolipooo.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 """Robot-joueur de Pooo
     
     Le module fournit les fonctions suivantes :
@@ -12,7 +11,6 @@
 
 __version__='0.1'
  
-
 ## chargement de l'interface de communication avec le serveur
 #from poooc import order, state, state_on_update, etime
 
@@ -21,27 +19,21 @@ import logging
 # pour faire de l'introspection
 import inspect
 
-
 def register_pooo(uid):
     """Inscrit un joueur et initialise le robot pour la compétition
-
         :param uid: identifiant utilisateur
         :type uid:  chaîne de caractères str(UUID) 
         
         :Example:
-        
         "0947e717-02a1-4d83-9470-a941b6e8ed07"
-
     """
     pass
 
 
 def init_pooo(init_string):
     """Initialise le robot pour un match
-        
         :param init_string: instruction du protocole de communication de Pooo (voire ci-dessous)
         :type init_string: chaîne de caractères (utf-8 string)
-       
        
        INIT<matchid>TO<#players>[<me>];<speed>;\
        <#cells>CELLS:<cellid>(<x>,<y>)'<radius>'<offsize>'<defsize>'<prod>,...;\
@@ -56,17 +48,12 @@ def init_pooo(init_string):
        De manière générale temps_de_trajet=<dist>/vitesse (division entière).
         
         :Example:
-        
         "INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"
-        
     """
     pass
     
-    
-    
 def play_pooo():
     """Active le robot-joueur
-    
     """
     logging.info('Entering play_pooo fonction from {} module...'.format(inspect.currentframe().f_back.f_code.co_filename))
     ### Début stratégie joueur ### 
