9014d68b827aa91ef96832f894b8976ef57e5a29
DGroeg <DGroeg@users.noreply.github.com>
2014-12-17 11:46:13 +0100

    Petits changements sur les classes / Greg

--- Game.py
+++ Game.py
@@ -1,33 +1,35 @@
 import Math
 
-class Game :
+class Game : #Terrain de la partie
     def __init__(self):
-        self.nodes = []
-        self.edges = []
+        self.nodes = [] #liste des cellules
+        self.edges = [] #liste des aretes
 
-class Node :
+class Node : #Cellule
     def __init__(self,x,y,id):
-    	self.x = x
-    	self.y = y
+    	self.x = x #Coordonnées
+    	self.y = y #Coordonnées
         self.id = id
-        self.owner = None
-        self.population = 0
-        self.production = 1
-        self.defense = 0
+        self.owner = None #Propriétaire
+        self.unitAtq = 0 #Nb d'unités en attaque
+        self.unitDef = 0 #Nb d'unités en défense
+        self.prodSpeedAtq = 1
+        self.prodSpeedDef = 1
+        self.maxUnit = 1 #Capacité de la case
 
-class Edge :
-    def __init__(self,id,s1,s2): #Arrête de s1 vers s2
+class Edge : #Arête
+    def __init__(self,id,s1,s2): #Arête de s1 vers s2
         self.id = id
         self.sommet1 = s1
         self.sommet2 = s2
-        self.length = Math.sqrt((s1.x-s2.x)**2+(s1.y-s2.y)**2)
-        self.pop = 0 #Combien d'unités parcourent cette arrête
-        self.owner = None #Qui possède ces unités
+        self.length = Math.sqrt((s1.x-s2.x)**2+(s1.y-s2.y)**2) #Longueur (temps ?)
+        self.fleets = [] #liste des Fleet qui la parcourent
 
-class Fleet :
-    def __init__(self,id,edge,to,nb):
+class Fleet : #Flotte en mouvement
+    def __init__(self,id,edge,to,size):
         self.id = id
-        self.edge = edge
-        self.to = to
-        self.nb = nb
-        self.progress = 0
+        self.owner = None #Qui possède ces unités
+        self.size = 1 #Taille
+        self.edge = 1 #sur quel arete elle se trouve
+        self.to = 0 #sa destination
+        self.progress = 0 #%age de son voyage accompli
