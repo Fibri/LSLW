bb29f06b36d08282fcd04b7067511f035d2408d6
Fibri <kevin.coepeau@gmail.com>
2015-01-16 17:33:38 +0000

    Verification du fonctionnement et insertion d'une stratégie simple

--- game.py
+++ game.py
@@ -1,86 +1,123 @@
-import Math
-
 class Game : #Terrain de la partie
-    def __init__(self,matchUid,playerUid,playerId,speed,cellNb,lineNb):
-        self.matchUid = matchUid
-        self.playerUid = playerUid
-        self.speed = speed
-        self.playerId = playerId
-        self.cellNb = cellNb
-        self.lineNb = lineNb
-        self.nodes = [] #liste des cellules
-        for i in range(cellNb+1):
-            self.nodes.append(0)
-        self.edges = [] #liste des aretes
-        for i in range(cellNb+1):
-            self.edges.append(0)
-    
-    def generateCells(self,cellDic):
-        for cell in cellDic:
-            self.nodes[cell['id']] = Node(cell['id'],cell['offsize'],cell['defsize'],cell['prod'])
+	def __init__(self,matchUid,playerUid,playerId,speed,cellNb,lineNb):
+		self.matchUid = matchUid
+		self.playerUid = playerUid
+		self.speed = speed
+		self.playerId = playerId
+		self.cellNb = cellNb
+		self.lineNb = lineNb
+		self.nodes = [] #liste des cellules
+		self.myPlayer = Player(playerId)
+		for i in range(cellNb+1):
+			self.nodes.append(0)
+		self.edges = [] #liste des aretes
+		for i in range(cellNb+1):
+			self.edges.append([])
+	
+	def generateCells(self,cellDic):
+		for cell in cellDic:
+			self.nodes[cell['id']] = Node(cell['id'],cell['offsize'],cell['defsize'],cell['prod'])
+			print("CREATE NODE WITH ID = ",cell['id'])
 
-    def generateLines(self,lineDic):
-        for line in lineDic:
-            self.edges[line['source']].append(Edge(self.nodes[line['source']],self.nodes[line['target']],line['dist']))
-            self.edges[line['target']].append(Edge(self.nodes[line['target']],self.nodes[line['source']],line['dist']))
-    
-    def updateCells(self,cellDic):
-        for updatedCell in cellDic:
-            self.nodes[updatedCell['id']].update()
+	def generateLines(self,lineDic):
+		for line in lineDic:
+			self.edges[line['source']].append(Edge(self.nodes[line['source']],self.nodes[line['target']],line['dist']))
+			self.edges[line['target']].append(Edge(self.nodes[line['target']],self.nodes[line['source']],line['dist']))
+			print("CREATE EDGE WITH SOURCE = ",line['source'], " AND TARGET ", line['target'])
+	
+	def updateCells(self,cellDic):
+		for updatedCell in cellDic:
+			self.nodes[updatedCell['id']].update(updatedCell['owner'],updatedCell['offunits'],updatedCell['defunits'])
 
-    def updateMoves(self,movDic):
-        for move in movDic:
-            for edge in self.edges:
-                if(edge['source'] == move['sourceId'] and edge['target'] == move['targetId']):
-                    edge.addFleet(Fleet(move['owner'],move['sourceId'],move['targetId'],move['unitNb']))
-                    break
+		self.myPlayer.clearNodes()
+		for node in self.nodes:
+			if(node == 0):
+				pass
+			else:
+				if(node.owner == self.playerId):
+					self.myPlayer.addNode(node)
 
-    def clearFleets(self):
-        for lineGrp in self.edges:
-            for line in lineGrp:
-                line.clearFleets()
+	def updateMoves(self,movDic):
+		for move in movDic:
+			for edge in self.edges[move['sourceId']]:
+				print("DEBUGGING 'EDGE ERROR': ",edge)
+				if(edge.source.id == move['sourceId'] and edge.target.id == move['targetId']):
+					edge.addFleet(Fleet(move['owner'],move['sourceId'],move['targetId'],move['unitNb']))
+					break
 
-    def generateNeighboors(self):
-        for node in self.nodes:
-            for edge in self.edges[node['id']]:
-                node.neighboors.append(self.nodes[edge['target']])
-        
+	def clearFleets(self):
+		for lineGrp in self.edges:
+			for line in lineGrp:
+				line.clearFleets()
+
+	def generateNeighboors(self):
+		for node in self.nodes:
+			if(node == 0):
+				pass
+			else:
+				for edge in self.edges[node.id]:
+					node.neighboors.append(edge.target)
+					node.neighboorsEdges.append(edge)
+					print("ADDING NEIGHBOOR WITH SOURCE = ", edge.source.id, " AND TARGET ", edge.target.id)
+				print("NUMBER OF NEIGHBOORS FOR ",node.id, " = ", len(node.neighboors))
+
+class Player :
+	def __init__(self,id):
+		self.id = id
+		self.myNodes = []
+
+	def clearNodes(self):
+		self.myNodes = []
+
+	def addNode(self,node):
+		self.myNodes.append(node)
 
 class Node : #Cellule
-    def __init__(self,id,offSize,defSize,prodSpeed):
-    	self.x = 0 #Coordonnées
-    	self.y = 0 #Coordonnées
-        self.id = id
-        self.owner = -1 #Propriétaire
-        self.unitAtq = 0 #Nb d'unités en attaque
-        self.unitDef = 0 #Nb d'unités en défense
-        self.maxAtq = offSize
-        self.maxDef = defSize
-        self.prodSpeedAtq = 1
-        self.prodSpeedDef = prodSpeed
-        self.neighboors = []
-        
-    def update(self,owner,offunits,defunits):
-        self.owner = owner
-        self.unitAtq = offunits
-        self.unitDef = defunits
+	def __init__(self,id,offSize,defSize,prodSpeed):
+		self.x = 0 #Coordonnées
+		self.y = 0 #Coordonnées
+		self.id = id
+		self.owner = -1 #Propriétaire
+		self.unitAtq = 0 #Nb d'unités en attaque
+		self.unitDef = 0 #Nb d'unités en défense
+		self.maxAtq = offSize
+		self.maxDef = defSize
+		self.prodSpeedAtq = 1
+		self.prodSpeedDef = prodSpeed
+		self.neighboors = []
+		self.neighboorsEdges = []
+		
+	def update(self,owner,offunits,defunits):
+		self.owner = owner
+		self.unitAtq = offunits
+		self.unitDef = defunits
+
+	def powerIn(self):
+		power = {}
+		for edgesIn in self.neighboorsEdges:
+			for fleet in edgesIn.fleets:
+				if(power[fleet.owner] == None):
+					power[fleet.owner] = fleet.size
+				else:
+					power[fleet.owner] += fleet.size
+		return power
 
 class Edge : #Arête
-    def __init__(self,s1,s2,time): #Arête de s1 vers s2
-        self.source = s1
-        self.target = s2
-        self.length = time #Longueur (temps en ms)
-        self.fleets = [] #liste des Fleet qui la parcourent
+	def __init__(self,s1,s2,time): #Arête de s1 vers s2
+		self.source = s1
+		self.target = s2
+		self.length = time #Longueur (temps en ms)
+		self.fleets = [] #liste des Fleet qui la parcourent
 
-    def clearFleets(self):
-        self.fleets = []
+	def clearFleets(self):
+		self.fleets = []
 
-    def addFleet(self,fleet):
-        self.fleets.append(fleet)
+	def addFleet(self,fleet):
+		self.fleets.append(fleet)
 
 class Fleet : #Flotte en mouvement
-    def __init__(self,owner,src,dst,size):
-        self.owner = owner #Qui possède ces unités
-        self.size = size #Taille
-        self.source = src #sur quelle arete elle se trouve
-        self.target = dst #sa destination
+	def __init__(self,owner,src,dst,size):
+		self.owner = owner #Qui possède ces unités
+		self.size = size #Taille
+		self.source = src #sur quelle arete elle se trouve
+		self.target = dst #sa destination
--- parse.py
+++ parse.py
@@ -1,218 +1,241 @@
 import re
+import math
 
 def parseCellsInit(str):
-    data = []
-    
-    cells = str.split('I,')
-    for i in range(len(cells)-1):
-        cells[i] = cells[i]+"I"
-    
-    """
-    <#cells>CELLS:<cellid>(<x>,<y>)'<radius>'<offsize>'<defsize>'<prod>,...;\
-    """
-    p = re.compile("(\d+)\((.*),(.*)\)'(\d+)'(\d+)'(\d+)'(\I*)")
-    
-    for cell in cells:
-        res = re.search(p, cell)
-        groups = res.groups()
-        
-        """
-        groups :
-        0 : id
-        1 : x
-        2 : y
-        3 : radius
-        4 : offsize
-        5 : defsize
-        7 : prod
-        """
-        
-        cellData = {
-        'id':int(groups[0]),
-        'x':int(groups[1]),
-        'y':int(groups[2]),
-        'radius':int(groups[3]),
-        'offsize':int(groups[4]),
-        'defsize':int(groups[5]),
-        'prod':len(groups[6])
-        }
-        
-        data.append(cellData)
-    
-    return data
-    
+	data = []
+	
+	cells = str.split('I,')
+	for i in range(len(cells)-1):
+		cells[i] = cells[i]+"I"
+	
+	"""
+	<#cells>CELLS:<cellid>(<x>,<y>)'<radius>'<offsize>'<defsize>'<prod>,...;\
+	"""
+	p = re.compile("(\d+)\((.*),(.*)\)'(\d+)'(\d+)'(\d+)'(\I*)")
+	
+	for cell in cells:
+		res = re.search(p, cell)
+		groups = res.groups()
+		
+		"""
+		groups :
+		0 : id
+		1 : x
+		2 : y
+		3 : radius
+		4 : offsize
+		5 : defsize
+		7 : prod
+		"""
+		
+		cellData = {
+		'id':int(groups[0]),
+		'x':int(groups[1]),
+		'y':int(groups[2]),
+		'radius':int(groups[3]),
+		'offsize':int(groups[4]),
+		'defsize':int(groups[5]),
+		'prod':len(groups[6])
+		}
+		
+		data.append(cellData)
+	
+	return data
+	
 def parseLinesInit(str):
-    data = []
-    
-    lines = str.split(',')
-    
-    """
-    <#lines>LINES:<cellid>@<dist>OF<cellid>,...
-    """
-    p = re.compile("(\d+)@(\d+)OF(\d+)")
-    
-    for line in lines:
-        res = re.search(p, line)
-        groups = res.groups()
-        
-        """
-        groups :
-        0 : source
-        1 : dist
-        2 : target
-        """
-        
-        lineData = {
-        'src':int(groups[0]),
-        'dist':int(groups[1]),
-        'target':int(groups[2])
-        }
-        
-        data.append(lineData)
-    
-    return data
-    
+	data = []
+	
+	lines = str.split(',')
+	
+	"""
+	<#lines>LINES:<cellid>@<dist>OF<cellid>,...
+	"""
+	p = re.compile("(\d+)@(\d+)OF(\d+)")
+	
+	for line in lines:
+		res = re.search(p, line)
+		groups = res.groups()
+		
+		"""
+		groups :
+		0 : source
+		1 : dist
+		2 : target
+		"""
+		
+		lineData = {
+		'source':int(groups[0]),
+		'dist':int(groups[1]),
+		'target':int(groups[2])
+		}
+		
+		data.append(lineData)
+	
+	return data
+	
 def parseCellsState(str):
-    data = []
-    
-    cells = str.split(',')
-    
-    """
-    <cellid>[<owner>]<offunits>'<defunits>,...;\
-    """
-    p = re.compile("(\d+)\[(\d+)\](\d+)'(\d+)")
-    
-    for cell in cells:
-        res = re.search(p, cell)
-        groups = res.groups()
-        
-        """
-        groups :
-        0 : id
-        1 : owner
-        2 : offunits
-        3 : defunits
-        """
-        
-        cellData = {
-        'id':int(groups[0]),
-        'owner':int(groups[1]),
-        'offunits':int(groups[2]),
-        'defunits':int(groups[3])
-        }
-        
-        data.append(cellData)
-    
-    return data
+	data = []
+	
+	cells = str.split(',')
+	
+	"""
+	<cellid>[<owner>]<offunits>'<defunits>,...;\
+	"""
+	p = re.compile("(\d+)\[(-{0,1}\d+)\](\d+)'(\d+)")
+	
+	for cell in cells:
+		res = re.search(p, cell)
+		try:
+			groups = res.groups()
+		except e:
+			print("EXCEPTION : ",cell)
+			raise e
+		
+		
+		"""
+		groups :
+		0 : id
+		1 : owner
+		2 : offunits
+		3 : defunits
+		"""
+		
+		cellData = {
+		'id':int(groups[0]),
+		'owner':int(groups[1]),
+		'offunits':int(groups[2]),
+		'defunits':int(groups[3])
+		}
+		
+		data.append(cellData)
+	
+	return data
 
 def parseMovesState(str):
-    data = []
-    
-    moves = str.split(',')
-    
-    """
-    <#moves>MOVES:<cellid><direction><#units>[<owner>]@<timestamp>'...<cellid>
-    """
-    
-    p = re.compile("(.*)'(\d+)$")
-    p2 = re.compile("(\d*)([<>])(\d+)\[(\d+)\]@(\d+)")
-    
-    for move in moves:
-        res = re.search(p, move)
-        groups = res.groups()
-        
-        movesInLine = groups[0].split("'")
-        currentLine = int(groups[1])
-        
-        for mov in movesInLine:
-            res2 = re.search(p2, mov)
-            groups = res2.groups()
-            
-            """
-            groups :
-            0 : targetId
-            1 : direction
-            2 : unitNb
-            3 : owner
-            4 : timestamp
-            """
-            if(groups[1] == '<'):
-                movData = {
-                    'targetId':int(groups[0]),
-                    'unitNb':int(groups[2]),
-                    'owner':int(groups[3]),
-                    'timestamp':int(groups[4]),
-                    'sourceId':currentLine
-                }
-            else:
-                movData = {
-                    'targetId':currentLine,
-                    'unitNb':int(groups[2]),
-                    'owner':int(groups[3]),
-                    'timestamp':int(groups[4]),
-                    'sourceId':None
-                }
-            data.append(movData)
-    
-    return data
+	data = []
+	if(str != ""):
+		moves = str.split(',')
+		
+		"""
+		<#moves>MOVES:<cellid><direction><#units>[<owner>]@<timestamp>'...<cellid>
+		"""
+		
+		p = re.compile("(.*)'(\d+)$")
+		p2 = re.compile("(\d*)([<>])(\d+)\[(\d+)\]@(\d+)")
+		
+		for move in moves:
+			res = re.search(p, move)
+			groups = res.groups()
+			
+			movesInLine = groups[0].split("'")
+			currentLine = int(groups[1])
+			lastTarget = None
+			
+			for mov in movesInLine:
+				res2 = re.search(p2, mov)
+				groups = res2.groups()
+				
+				"""
+				groups :
+				0 : targetId
+				1 : direction
+				2 : unitNb
+				3 : owner
+				4 : timestamp
+				"""
+				if(groups[1] == '<'):
+					if(groups[0] != ''):
+						lastTarget = int(groups[0])
+					movData = {
+						'targetId':lastTarget,
+						'unitNb':int(groups[2]),
+						'owner':int(groups[3]),
+						'timestamp':int(groups[4]),
+						'sourceId':currentLine
+					}
+				else:
+					movData = {
+						'targetId':currentLine,
+						'unitNb':int(groups[2]),
+						'owner':int(groups[3]),
+						'timestamp':int(groups[4]),
+						'sourceId':int(groups[0])
+					}
+				data.append(movData)
+		
+	return data
 
 def analyzeState(str,matchid):
-    """
-    STATE<matchid>IS<#players>;
-    <#cells>CELLS:<cellid>[<owner>]<offunits>'<defunits>,...;\
-    <#moves>MOVES:<cellid><direction><#units>[<owner>]@<timestamp>'...<cellid>,...
-    """
-    
-    p = re.compile("^([A-Z]*)")
-    
-    res = re.search(p, str)
-    groups = res.groups()
-    
-    state_type = groups[0]
-    
-    if (state_type == 'STATE'):
-        p = re.compile("STATE(.*)IS(\d+);(\d+)CELLS:(.*);(\d+)MOVES:(.*)")
-        
-        res = re.search(p, str)
-        groups = res.groups()
+	"""
+	STATE<matchid>IS<#players>;
+	<#cells>CELLS:<cellid>[<owner>]<offunits>'<defunits>,...;\
+	<#moves>MOVES:<cellid><direction><#units>[<owner>]@<timestamp>'...<cellid>,...
+	"""
+	
+	p = re.compile("^([A-Z]*)")
+	
+	res = re.search(p, str)
+	groups = res.groups()
+	
+	state_type = groups[0]
+	
+	if (state_type == 'STATE'):
+		p = re.compile("STATE(.*)IS(\d+);(\d+)CELLS:(.*);(\d+)MOVES:{0,1}(.*)")
+		
+		res = re.search(p, str)
+		groups = res.groups()
 
-        if(groups[0] == matchid):
-            stateData = {
-                'matchid':groups[0],
-                'playerNb':int(groups[1]),
-                'cellNb':int(groups[2]),
-                'cellData':parseCellsState(groups[3]),
-                'lineNb':int(groups[4]),
-                'lineData':parseMovesState(groups[5])
-            }
-            
-            returnType = {
-                'type':state_type,
-                'data':stateData
-            }
-    elif (state_type == 'GAMEOVER'):
-        returnType = {
-            'type':state_type
-        }
-        #créer une fonction (message de victoire si c'est nous qui gagnons ^^)
-        
-    elif (state_type == 'ENDOFGAME'):
-        returnType = {
-            'type':state_type
-        }
-        #se déconnecter
-    return returnType
+		if(groups[0] == matchid):
+			stateData = {
+				'matchid':groups[0],
+				'playerNb':int(groups[1]),
+				'cellNb':int(groups[2]),
+				'cellData':parseCellsState(groups[3]),
+				'moveNb':int(groups[4]),
+				'moveData':parseMovesState(groups[5])
+			}
+			
+			returnType = {
+				'type':state_type,
+				'data':stateData
+			}
+
+	elif (state_type == 'GAMEOVER'):
+		#GAMEOVER[1]20ac18ab-6d18-450e-94af-bee53fdc8fca
+		p = re.compile("GAMEOVER\[(\d+)\](.*)")
+		
+		res = re.search(p, str)
+		groups = res.groups()
+
+		stateData = {
+			'matchid':groups[1],
+			'winnerId':int(groups[0])
+		}
+		
+		returnType = {
+			'type':state_type,
+			'data':stateData['winnerId']
+		}
+		#créer une fonction (message de victoire si c'est nous qui gagnons ^^)
+		
+	elif (state_type == 'ENDOFGAME'):
+		returnType = {
+			'type':state_type
+		}
+		#se déconnecter
+	return returnType
 
 def createMoveOrder(cellFrom,cellToId,cellNb,playerId):
-    sourceSize = cellFrom['offunits']
-    prct = cellNb/sourceSize*100
-    if(prct > 100):
-        prct = 100
-    
-    
-    #[<userid>]MOV<%offunits>FROM<cellid>TO<cellid>
-    #[0947e717-02a1-4d83-9470-a941b6e8ed07]MOV33FROM1TO4
-    cmd = "["+playerId+"]MOV"+prct+"FROM"+cellFrom['id']+"TO"+cellToId
-    
-    return cmd
-    
+	sourceSize = cellFrom.unitAtq
+	prct = int(math.ceil(cellNb/sourceSize*100))
+	if(prct > 100):
+		prct = 100
+	
+	
+	#[<userid>]MOV<%offunits>FROM<cellid>TO<cellid>
+	#[0947e717-02a1-4d83-9470-a941b6e8ed07]MOV33FROM1TO4
+	cmd = "["+str(playerId)+"]MOV"+str(prct)+"FROM"+str(cellFrom.id)+"TO"+str(cellToId)
+	print("SEND MOVE ORDER : ", cmd)
+	
+	return cmd
+	
